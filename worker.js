7c22da7e164270f0d43660714e214504ecf9abef
felivicente03-code <felivicente03@gmail.com>
2026-03-08 10:45:42 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,32 @@
+export default {
+  async fetch(request, env) {
 
+    if (request.method !== "POST") {
+      return new Response("Bot activo");
+    }
+
+    const data = await request.json();
+
+    if (data.message) {
+      const chatId = data.message.chat.id;
+      const text = data.message.text;
+
+      if (text === "/start") {
+
+        await fetch(`https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: "Bot funcionando en Cloudflare 🚀"
+          })
+        });
+
+      }
+    }
+
+    return new Response("ok");
+  }
+}
