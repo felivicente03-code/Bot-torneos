8f81e2ecbee919000642765d4dd9db13a3cd5ad7
felivicente03-code <felivicente03@gmail.com>
2026-03-08 21:35:46 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,5 @@
 export default {
-  async fetch(request, env) {
-    const { results } = await env.MY_DB.prepare(
-      "SELECT * FROM Customers LIMIT 100"
-    ).run();
-
-    return new Response(JSON.stringify(results));
+  async fetch(request) {
+    return new Response("Worker activo");
   }
 };
