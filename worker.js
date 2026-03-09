95d985f14da5fb2854bbbee5e382cd4a03cef530
felivicente03-code <felivicente03@gmail.com>
2026-03-09 18:40:28 -0300

    Actualizar worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,10 @@
+const TOKEN = "8750689884:AAGX4mL-lUxs-5zEbgONWvyzFW6bXDiJB3A";
+const ADMIN_ID = 8581898801;
+
+
+
+
+
 export default {
   async fetch(request, env) {
 
@@ -9,6 +16,37 @@ export default {
 
         const chat_id = data.message.chat.id;
         const text = data.message.text;
+        const user_id = data.message.from.id;
+if (text && text.toLowerCase() === "torneo") {
+
+  const torneos = await env.torneos_db.prepare(
+    "SELECT id, nombre FROM torneos"
+  ).all();
+
+  const botones = torneos.results.map(t => [{
+    text: t.nombre,
+    callback_data: "torneo_" + t.id
+  }]);
+
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: chat_id,
+      text: "🏆 Hola!!!\n¿A cuál torneo quieres inscribirte? 🎮",
+      reply_markup: {
+        inline_keyboard: botones
+      }
+    })
+  });
+
+}
+
+if (text && text.startsWith("/") && user_id != ADMIN_ID) {
+  return new Response("No autorizado");
+}
 
         if (text === "/torneo") {
 
