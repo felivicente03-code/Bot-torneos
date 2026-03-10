66d994b80ab3a04017092acc929077ecb0888443
felivicente03-code <felivicente03@gmail.com>
2026-03-09 23:02:27 -0300

    Actualizar worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 const TOKEN = "8750689884:AAGX4mL-lUxs-5zEbgONWvyzFW6bXDiJB3A";
 
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
 
     if (request.method !== "POST") {
       return new Response("Bot activo");
@@ -14,14 +14,24 @@ export default {
       const chat_id = data.message.chat.id;
       const text = data.message.text;
 
-      if (text === "/start") {
+      if (text === "torneo") {
+
+        const torneos = await env.DB.prepare(
+          "SELECT id, nombre FROM torneos"
+        ).all();
+
+        let mensaje = "🏆 Torneos disponibles:\n\n";
+
+        torneos.results.forEach(t => {
+          mensaje += `${t.id} - ${t.nombre}\n`;
+        });
 
         await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             chat_id: chat_id,
-            text: "Hola mundo"
+            text: mensaje
           })
         });
 
