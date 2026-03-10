456a79a9b684da7fb5a1bd93087f9dfe29b043db
felivicente03-code <felivicente03@gmail.com>
2026-03-10 20:40:19 -0300

    Actualizar worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
 const TOKEN = "8750689884:AAGX4mL-lUxs-5zEbgONWvyzFW6bXDiJB3A";
 
-
 export default {
   async fetch(request) {
     if (request.method !== "POST") return new Response("Bot activo");
@@ -11,17 +10,15 @@ export default {
       if (data.message) {
         const chat_id = data.message.chat.id;
 
-        // Respuesta rápida
-        fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+        await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             chat_id: chat_id,
             text: "👋 Hola, funcionó el bot!"
           })
-        }).catch(err => console.log("Error enviando mensaje:", err));
+        });
 
-        // Telegram recibe respuesta inmediatamente
         return new Response("ok");
       }
 
